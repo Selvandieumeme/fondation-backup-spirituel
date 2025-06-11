2f9a2d030094be78d0afb2801b648241a9a1e170
Selvandieumeme <infos@fondationbackupspirituel.com>
2025-06-11 03:11:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // Fè sa yon sèl fwa an tèt
 
 
 // 👇 Test si .env byen chaje
-console.log("MONGO_URI =>", process.env.MONGO_URI);
+console.log("MONGODB_URI =>", process.env.MONGODB_URI);
 
 const express = require("express");
 const Pusher = require("pusher");
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // 🔗 Koneksyon ak MongoDB (an sekirite)
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
